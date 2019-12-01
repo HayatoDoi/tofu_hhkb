c72c3e26615f0e504a7d6cbe470b0622cbbbfbdf
HayatoDoi <hayato.doi@mail.exploit.moe>
2019-12-01 11:35:55 +0900

    change product name.

--- config.h
+++ config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    You
-#define PRODUCT         kbd6x
+#define PRODUCT         tofu hhkb keyborad (doi model)
 #define DESCRIPTION     A custom keyboard
 
 /* key matrix size */
